baeb10d4d9ea698ff6973b2facb1e5be88f57b4d
Rawser Spicer <rwspicer@alaska.edu>
2014-08-14 07:21:36 -0800

    updated rh_calculator to use the utility class

--- rh_calculator.py
+++ rh_calculator.py
@@ -3,7 +3,10 @@ relative humidity calulator
 rh_calculator.py
 Rawser Spicer
 created: 2014/01/24
-modified: 2014/08/08
+modified: 2014/08/14
+
+    version 2014.8.14.1:
+        now uses the utility class
 
     version 2014.8.8.1
         upadated docs
@@ -33,8 +36,9 @@ modified: 2014/08/08
 """
 import math
 import csv_lib.csv_file as csvf
-import csv_lib.csv_args as csva
-from csv_lib.csv_utilities import print_center, exit_on_success, exit_on_failure
+#import csv_lib.csv_args as csva
+#from csv_lib.csv_utilities import print_center, exit_on_success, exit_on_failure
+import csv_lib.utility as util
 
 
 def calc_rh(at_date, at_val, dp_date, dp_val):
@@ -120,10 +124,108 @@ def main():
         print_center("No data was written.                         ")      
 
     exit_on_success()
+
+
+
+class rh_util(util.utility_base):  
+    """ 
+        this class is used as a relative humiditiy calculator.
+    """
+
+    def __init__(self):
+        """
+            sets up utility
+        """
+        super(rh_util, self).__init__("Realative Humidity Calculator" ,
+                    ("--at_file", "--dp_file", "--rh_file") ,(), "help")
+                    
+        self.help =  """
+        To correctly use this python utility:
+        $ python track_delay.py --at_file=<path>/filename.csv
+        --dp_file=<path>/filename.csv --rh_file=<path>/filename.csv
+        
+        """
+       
+    def load_files(self):
+        """
+            loads the files form the names in the arguments
+        """
+        try:
+            self.at_file = csvf.CsvFile(self.commands["--at_file"], True)
+            err = "--at_file"
+            self.dp_file = csvf.CsvFile(self.commands["--dp_file"], True)
+            err = "--dp_file"
+            self.rh_file = csvf.CsvFile(self.commands["--rh_file"])
+            err = "--rh_file"     
+        except IOError:
+            self.errors.set_error_state("File Not Found", 
+                            "A required file was not found",
+                            err)
+        if not self.rh_file.exists():
+            self.print_center(self.rh_file.name() + \
+                                        " not found, it will be created")
+            temp = self.at_file.get_header()
+            temp[1][1] = "Relative Humidity\n"
+            temp[2][1] = "Percent\n"
+            self.rh_file.set_header(temp)   
+        else:
+            self.print_center(self.rh_file.name() + \
+                                     " found, it will be appended to") 
+        
+        
+                            
+    def calc_rh(self):    
+        """
+            calculates the relative humiditys
+        """
+        comp_val = 6999
+        air_dates = self.at_file[0]
+        air_temps = self.at_file[1]
+        dew_dates = self.dp_file[0]
+        dew_points = self.dp_file[1]
+        self.rh_vals = []
+        index = 0
+        while (index < len(air_dates)):
+            rh_val = comp_val
+            if not(air_dates[index] == dew_dates[index]):
+                errors.set_error_state("Date", 
+                        "the air temp and dew point dates do not match")
+                return
+                # how to handle this?
+            if (air_temps[index] != comp_val and 
+                dew_points[index] != comp_val):
+                denominator = 0.611 * math.exp((17.3 * air_temps[index])
+                                 / (air_temps[index] + 237.3))
+                numerator = 0.611 * math.exp((17.3 *  dew_points[index]) 
+                                / ( dew_points[index] + 237.3))
+                rh_val = numerator / denominator * 100    
+            self.rh_vals.insert(index, rh_val)
+            index = index + 1
+    
+    def main(self):
+        """
+            the body of the utility
+        """
+        self.load_files()
+        self.calc_rh()
+        if self.errors.get_error_state():
+            self.errors.print_errors()
+            self.exit()
+        
+        self.rh_file.set_dates(self.at_file[0])
+        self.rh_file[1] = self.rh_vals
+        
+        if not self.rh_file.append():
+            self.print_center("Old dates indcate no new data to be appended.") 
+            self.print_center("No data was written.                         ")    
+       
+        
+        
  
 
 if __name__ == "__main__":
-    main()
+    rhu = rh_util()
+    rhu.run()
 
     
     
