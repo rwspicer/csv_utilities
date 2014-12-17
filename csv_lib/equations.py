8fd2b54669e51f3ce62c7fc553db52b3bd253937
Rawser Spicer <rwspicer@alaska.edu>
2014-12-17 10:35:32 -0900

    fixed typos in comments

--- csv_lib/equations.py
+++ csv_lib/equations.py
@@ -10,7 +10,7 @@ Part of DataPro Version 3
     
     version 2014.9.4.1:
         updated documentation and each class now allows any type converable to 
-    float to be passed as an argument to varible
+    float to be passed as an argument to variable
     
     version 2014.9.29.1:
         fixed minor errors
@@ -22,16 +22,16 @@ class   equation(object):
     """
         this servs as a base class for equations
     """
-    def __init__(self, varible, bad_data_val = 6999):
+    def __init__(self, variable, bad_data_val = 6999):
         """ 
-        Class initialiser  
+        Class initializer  
         
         arguments:
-           varible:        (convertable to float) the resistance varible value
-           bad_val         (convertable to int) the value to indicate a 
+           variable:        (convertible to float) the resistance variable value
+           bad_val         (convertible to int) the value to indicate a 
                         bad data item   
         """
-        self.varible = float(varible)
+        self.variable = float(variable)
         self.bad_value = int(bad_data_val)
         self.result = "not calculated"
         self.calc()
@@ -55,56 +55,56 @@ class thermistor(equation):
     """
         This class is for processing thermistor values using the 
     Steinhart-Hart equation. This class takes a resistance and cofficents and
-    calculates a temperature into the resulat varible
+    calculates a temperature into the resulat variable
     """
-    def __init__(self, varible, a, b, c, offset = 0 , bad_val = 6999):
+    def __init__(self, variable, a, b, c, offset = 0 , bad_val = 6999):
         """ 
-        Class initialiser  
+        Class initializer  
         
         arguments:
-            varible:        (convertable to float) the resistance varible value
-            a, b, c:        (convertable to float) the Stienhart-Hart 
+            variable:        (convertible to float) the resistance variable value
+            a, b, c:        (convertible to float) the Stienhart-Hart 
                         coefficents
-            offset:         (convertable to float) a correctional offset
-            bad_val:        (convertable to int) the value to indicate a 
+            offset:         (convertible to float) a correctional offset
+            bad_val:        (convertible to int) the value to indicate a 
                         bad data item  
         """
         self.A = float(a)
         self.B = float(b)
         self.C = float(c)
         self.offset = float(offset)
-        super(thermistor, self).__init__(varible, bad_val)
+        super(thermistor, self).__init__(variable, bad_val)
         
     
     def calc(self):
         """
         Calculates the Steinhart-Hart conversion from resastanc to temperature
         """
-        if abs(self.varible) >= 6999 or self.varible <=0:
+        if abs(self.variable) >= 6999 or self.variable <=0:
             self.result = self.bad_value
             return
         
-        r = self.varible * 1000
+        r = self.variable * 1000
         self.result = (1 / (self.A + self.B * log(r) + self.C * log(r) ** 3)\
                         - 273.15) + self.offset
 
 
 class poly(equation):
     """ 
-        This class represents a polynomial function with a varible number of 
-    terms. The cofficent for each term should be pased in a tuple containing the 
-    coefficents value at the power term it will be used at. 
+        This class represents a polynomial function with a variable number of 
+    terms. The coefficient for each term should be passed in a tuple containing 
+    the coefficients value at the power term it will be used at. 
     (ie. 4x^2 + 1 is (1 , 0, 4), or 5x^3 is (0, 0, 0, 5)   
     """
     
     def __init__ (self, var, coefs, bad_val = 6999):
         """ 
-        Class initialiser
+        Class initializer
         
         Arguments:
-            var:            (convertable to float)  the domain value
+            var:            (convertible to float)  the domain value
             coefs:          (tuple of numbers) the cofficents for the polynomial
-            bad_val:        (convertable to int) the value to indicate a 
+            bad_val:        (convertible to int) the value to indicate a 
                         bad data item  
         """
         self.coefs = coefs
@@ -114,13 +114,13 @@ class poly(equation):
         """
         Calculates the polynomial function value
         """
-        if abs(self.varible) >= 6999:
+        if abs(self.variable) >= 6999:
             self.result = self.bad_value
             return
         
         temp = 0 
         for idx in range(len(self.coefs)):
-            temp += float(self.coefs[idx]) * self.varible ** idx
+            temp += float(self.coefs[idx]) * self.variable ** idx
         self.result = temp
         
         
@@ -131,15 +131,15 @@ class flux(equation):
     
     def __init__ (self, var, posical, negical, bad_val = 6999):
         """ 
-        Class initialiser 
+        Class initializer 
         
         Arguments:
-            var:            (convertable to float)  the domain value
-            posical:        (convertable to float) the multiplier for positive 
+            var:            (convertible to float)  the domain value
+            posical:        (convertible to float) the multiplier for positive 
                         values        
-            negical:        (convertable to float) the multiplier for negative 
+            negical:        (convertible to float) the multiplier for negative 
                         values   
-            bad_val:        (convertable to int) the value to indicate a 
+            bad_val:        (convertible to int) the value to indicate a 
                         bad data item  
         """
         self.posical = float(posical)
@@ -151,14 +151,14 @@ class flux(equation):
         """
         calculates the flux into the result member 
         """
-        if abs(self.varible) >= 6999:
+        if abs(self.variable) >= 6999:
             self.result = self.bad_value
             return
         
-        if self.varible >= 0:
-            self.result = self.posical * self.varible
+        if self.variable >= 0:
+            self.result = self.posical * self.variable
         else:
-            self.result = self.negical * self.varible
+            self.result = self.negical * self.variable
         
 class netrad(equation):
     """
@@ -167,16 +167,16 @@ class netrad(equation):
     
     def __init__ (self, var, windspeed, posical, negical, bad_val = 6999):
         """ 
-        Class initialiser 
+        Class initializer 
         
         Arguments:
-            var:            (convertable to float)  the domain value
-            windspeed:      (convertable to float)
-            posical:        (convertable to float) the multiplier for positive 
+            var:            (convertible to float)  the domain value
+            windspeed:      (convertible to float)
+            posical:        (convertible to float) the multiplier for positive 
                         values        
-            negical:        (convertable to float) the multiplier for negative 
+            negical:        (convertible to float) the multiplier for negative 
                         values   
-            bad_val:        (convertable to int) the value to indicate a 
+            bad_val:        (convertible to int) the value to indicate a 
                         bad data item  
         """
         self.posical = float(posical)
@@ -188,7 +188,7 @@ class netrad(equation):
         """
         calcualts the netrad in to the result memver
         """
-        if abs(self.varible) >= self.bad_value:
+        if abs(self.variable) >= self.bad_value:
             self.result = self.bad_value
             return
             
@@ -196,14 +196,14 @@ class netrad(equation):
             pos_correction = 1 + (.066 * .2 * self.windspeed)/ \
                                 (.066 + (.2 * self.windspped))
             neg_correction = (.00174 * self.windspeed) + .99755
-            uncorrected = flux(self.varible, self.posical, self.negical,
+            uncorrected = flux(self.variable, self.posical, self.negical,
                                              self.badvalue).result
             if uncorrected >= 0:
                 self.result = uncorrecte * pos_correction
             else:
                 self.result = uncorrecte * neg_correction
         else:
-            self.result = flux(self.varible, self.posical, self.negical,
+            self.result = flux(self.variable, self.posical, self.negical,
                                              self.badvalue).result
 
 
@@ -215,14 +215,14 @@ class rt_sensor(equation):
     
     def __init__ (self, var, div, offset, mult, bad_val = 6999):
         """ 
-        Class initialiser 
+        Class initializer 
         
         Arguments:
-            var:            (convertable to float) the domain value
-            div:            (convertable to float) a divisor
-            offset:         (convertable to float) an offset
-            mult:           (convertable to float) a multiplier
-            bad_val:        (convertable to int) the value to indicate a 
+            var:            (convertible to float) the domain value
+            div:            (convertible to float) a divisor
+            offset:         (convertible to float) an offset
+            mult:           (convertible to float) a multiplier
+            bad_val:        (convertible to int) the value to indicate a 
                         bad data item  
         """
         self.div = float(div)
@@ -235,8 +235,8 @@ class rt_sensor(equation):
         """
         calculates the calibration
         """
-        if abs(self.varible) >= 6999:
+        if abs(self.variable) >= 6999:
             self.result = self.bad_value
             return
     
-        self.result = ((self.varible / self.div) + self.offset) / self.mult
+        self.result = ((self.variable / self.div) + self.offset) / self.mult
