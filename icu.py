ddfbaa8102cee26e56f9d0a679fad52b8024cd3b
rawser spicer <rwspicer@alaska.edu>
2015-03-05 12:39:28 -0900

    started interval checting utiity

---
+++ icu.py
@@ -0,0 +1,20 @@
+"""
+icu.py
+
+interval checking utility
+
+IARC data processing project
+
+rawser spicer
+created: 2015/03/05
+modified: 2014/03/05
+
+	This utility can check a csv file, in the datapro output format, for any 
+entries that do not lie on a standard interval provided as input.
+"""
+
+
+
+class ICU(util.utility_base):
+	def __init__(self):
+		pass
