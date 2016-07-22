07ed299a37503c7f1ad1f19be3a464427974fbe0
Bob Busey <rcbusey@alaska.edu>
2016-07-22 22:10:48 +0000

    Rearranged qc_funk() so that the xls file can contain either bad data designators or good data substitutes.

--- qc.py
+++ qc.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-qc utility 
+qc utility
 qc.py
 Rawser Spicer
 created: 2014/03/13
@@ -9,13 +9,13 @@ modified: 2014/04/14
        this utility preforms quality based on a file with values to compare
     this program uses the xlrd package to interface wiht excel files:
     http://www.python-excel.org/
-    
+
     version 2014.8.8.1:
         updated docs
 
     version 2013.4.14.1:
-        added documetation 
-  
+        added documetation
+
 """
 from csv_lib.csv_utilities import print_center, exit_on_failure, exit_on_success
 import csv_lib.csv_file as csvf
@@ -27,22 +27,22 @@ import math
 def fetch_excel_data(file_name):
     """
         reads excel file data
-    
+
     arguments:
         file_name:      (string) the file to read
-    
-    returs 
+
+    returs
         a list of two lists [[dates],[values]]
     """
     wb = xlrd.open_workbook(file_name)
     sheet = wb.sheet_by_index(0)
     n_rows = sheet.nrows
-    n_cols = 2#sheet.ncols
+    n_cols = 2 #sheet.ncols
     o_val = [[],[]]
     for c_index in range(n_cols):
         for r_index in range(n_rows):
             if r_index < 4:
-                continue          
+                continue
             temp = sheet.cell(r_index,c_index)
             if c_index == 0:
                 temp.value = datetime.datetime(*xlrd.xldate_as_tuple(temp.value,
@@ -54,14 +54,14 @@ def fetch_excel_data(file_name):
 def is_bv(val):
     """
         checks for a bad value
-    
+
     arguments:
         val:    (float) value to check
-    
-    returns: 
+
+    returns:
         true if the value is a bad value
     """
-    if math.isnan(val):
+    if math.isnan(float(val)):
         return True
     if val == 6999 or val == 7777 or val == 9999:
         return True
@@ -70,25 +70,29 @@ def is_bv(val):
 #todo close f_stream
 def qc_func(in_data, xl_data, log_file):
     """
-        preforms the quality controll check
-    
+        performs the quality control check
+
     arguments:
         in_data:    (list) data to check
         xl_data:    (list) data to check with
         log_file:   (string) a file to log changes
-    
-    returns: 
+
+    returns:
         fixed data in [[dates],[values]] form
-    """    
-    f_stream = open(log_file, 'a')    
+    """
+    f_stream = open(log_file, 'a')
 
     for index, items in enumerate(in_data[0]):
         for xidx, xitm in enumerate(xl_data[0]):
-            if items == xitm and is_bv(xl_data[1][xidx]):
-                f_stream.write(str(items) + ",Manual QC, Orignal:" + \
-                                str(in_data[1][index]) + " Replacment: " + \
-                                str(xl_data[1][xidx]) + "\n")
-                in_data[1][index] = xl_data[1][xidx]
+            #print items, xitm
+            if items == xitm :
+                # check to see if the data has been set to bad
+                # or if it has been replaced by something else legit.
+                if is_bv(xl_data[1][xidx]) or (abs(in_data[1][index] - xl_data[1][xidx]) < 0.01 ):
+                    f_stream.write(str(items) + ",Manual QC, Orignal:" + \
+                                    str(in_data[1][index]) + " Replacment: " + \
+                                    str(xl_data[1][xidx]) + "\n")
+                    in_data[1][index] = xl_data[1][xidx]
     return in_data[:]
 
 
@@ -101,29 +105,29 @@ HELP_STR = """
             --short: a correction file (excel format)
             --insitu: a correction file (excel format)
 
-            note: use --short or --insitu, not both 
-           """ 
+            note: use --short or --insitu, not both
+           """
 
 def main():
     """the utility"""
     print_center(UTILITY_NAME, '-')
-    
-    try:    
+
+    try:
         commands = csva.ArgClass(REQ_FLAGS, OPT_FLAGS, HELP_STR)
     except RuntimeError, (error_message):
-        exit_on_failure(error_message[0]) 
-    
+        exit_on_failure(error_message[0])
+
     if commands.is_missing_flags():
         for items in commands.get_missing_flags():
             print_center(" ERROR: flag <" + items + "> is required ", "*")
-        exit_on_failure()    
+        exit_on_failure()
 
     try:
         in_file = csvf.CsvFile(commands["--input"], True)
     except IOError:
         print_center("ERROR: a required file was not found", '*')
         exit_on_failure()
-    
+
     try:
         xl_data = fetch_excel_data(commands["--insitu"])
     except KeyError:
@@ -140,16 +144,16 @@ def main():
         exit_on_failure()
 
     in_data = [in_file[0], in_file[1]]
-    
+
     name = commands["--input"].split('/')[-1].split('.')[0]
-    path_str = ""    
-    for items in commands["--input"].split('/')[:-2]: 
+    path_str = ""
+    for items in commands["--input"].split('/')[:-2]:
         path_str += items + '/'
     log_file = path_str + 'qc/' + name + "_qaqc_log.csv"
-    
+
     fixed_data = qc_func(in_data, xl_data, log_file)
-    
-        
+
+
     in_file[0] = fixed_data[0]
     in_file[1] = fixed_data[1]
 
